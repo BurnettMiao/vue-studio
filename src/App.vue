5b141dc6b30aa323e8db1eb9bf1923973c6d42a9
BurnettMiao <searchfor1987@gmail.com>
2024-06-17 22:21:04 +0800

    20240617測試

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@ import FooterSection from '@/components/FooterSection.vue'
 <template>
   <NavbarSection />
 
-  <h1>一頁書 Studio</h1>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, suscipit nesciunt
-    quibusdam possimus tempore vel veniam, ipsam id impedit ullam perferendis atque nostrum, sequi
-    consectetur eum eaque voluptatum excepturi ut!
-  </p>
+  <div class="container">
+    <h1>一頁書 Studio</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, suscipit nesciunt
+      quibusdam possimus tempore vel veniam, ipsam id impedit ullam perferendis atque nostrum, sequi
+      consectetur eum eaque voluptatum excepturi ut!
+    </p>
+  </div>
 
   <FooterSection />
 </template>
--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>我是Footer Section</div>
+  <div class="container">
+    <h1>我是Footer Section</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nostrum rem voluptate et iste
+      quo. Porro quod at iure maiores accusamus illum perferendis repellat deserunt, impedit
+      laudantium odio sint voluptates?
+    </p>
+  </div>
 </template>
 
 <script setup></script>
